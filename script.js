cb6e9d344f0697586953296e2c0dd7467b6305a8
Olsahaliti <olsahaliti931@gmail.com>
2024-12-03 09:43:44 -0800

    punova

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-for (let i = 9;i>4; i--) {
-    console.log(i)
-}
+// for (let i = 9;i>4; i--) {
+//     console.log(i)
+// }
 
-for( let i = 0; i <= 100; i=+2) {
-    console.log(i)
-}
+// for( let i = 0; i <= 100; i= ++ ) {
+//     console.log(i)
+// }
+
+// for(let i = 1; i <=8; i++) { 
+//     if (i ===7) { continue
+// }
+// console.log(i) 
+// }
+
+
+ let start,_stop;
+
+ start=prompt("shkruaj numrin e pare")
+ _stop=prompt("shkruaj numrin e dyte")
+
+ for (let i = start; i <= _stop; i++) {
+    console.log(i);
+ }
+
+
+
+
+
